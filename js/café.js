a194b6a7e22c017ad6ec809bd10cedb18a89c15d
Selma-mtch <mataiche.selma77@gmail.com>
2024-06-26 21:47:32 +0200

    Add files via upload

---
+++ js/café.js
@@ -0,0 +1,17 @@
+let photoCadre=document.querySelectorAll('.cadrePhoto');
+
+for (let i=0;i<photoCadre.length; i++ ){
+    photoCadre[i].addEventListener('mouseover',opacité0);
+    photoCadre[i].addEventListener('mouseout',opacité1);
+    
+};
+
+function opacité0(event) {
+    if (event){this.style.opacity=0.5;}
+    else{this.style.opacity=1;}
+};
+
+function opacité1(event) {
+    if (event){this.style.opacity=1;}
+    else{this.style.opacity=0;}
+};
